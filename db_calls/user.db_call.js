e5177a04757bc1d0062dfeb05eb31d3d9abdfa6a
nomercy0 <aagarwal063@gmail.com>
2018-07-31 16:06:24 +0530

    user api changed

--- db_calls/user.db_call.js
+++ db_calls/user.db_call.js
@@ -32,6 +32,9 @@ exports.addNew = function(userObj) {
 exports.getByUsername = function(userObj) {
 	return new Promise(function(resolve,reject){
 		User.findOne({where:{ username:userObj.username,password:userObj.password}}).then(function(user){
+			if(!user){
+				return reject(new Error('No user found '))
+			}
 			resolve(user);
 		}).catch(function(err){
 			reject(err);
