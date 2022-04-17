638b3a774a814234d6aeaadf5f021671f10d4ad9
alter-sachin <guitarplayersachin@gmail.com>
2022-04-17 10:25:34 +0000

    changed prod database name

--- config/env/production.js
+++ config/env/production.js
@@ -3,7 +3,7 @@ module.exports = {
 		host : process.env.dbHost || 'localhost',
 		user : process.env.dbUser || 'root',
 		password: process.env.dbPassword || '',
-		database: process.env.dbName || 'crispAR-prod'
+		database: process.env.dbName || 'crispARprod'
 	},
 	port: process.env.PORT || 8443,
 	host: process.env.HOST || '0.0.0.0'
